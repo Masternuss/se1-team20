660c5214974055c30a01cedce3a557540cd6c044
bella <a.ab@mail.de>
2021-04-23 16:25:28 +0200

    "just foolling around"

---
+++ test_folder/Time.java
@@ -0,0 +1,13 @@
+package testing;
+
+public class Time implements TimeIF{
+    @Override
+    public int hours() {
+        return 0;
+    }
+
+    @Override
+    public int minutes() {
+        return 0;
+    }
+}
