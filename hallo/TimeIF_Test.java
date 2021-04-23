cc8b8188b5102f2c67e476111964c6bc941b987e
bella <a.ab@mail.de>
2021-04-23 16:50:32 +0200

    "hallo"

---
+++ hallo/TimeIF_Test.java
@@ -0,0 +1,34 @@
+package testing;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeIF_Test {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void hours() {
+    }
+
+    @Test
+    void minutes() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
