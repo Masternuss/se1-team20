3e5185ea34c01b62d3d042ba7bc58ff96b489bd2
1nbetw33n <a.ab@mail.de>
2021-05-08 00:17:26 +0200

    UnitTest added. Not sure how to actually test the Servlet.

--- src/main/java/com/example/Parkhaus/Parkhaus_UnitTest.java
+++ src/main/java/com/example/Parkhaus/Parkhaus_UnitTest.java
@@ -1,4 +1,27 @@
 package com.example.Parkhaus;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class Parkhaus_UnitTest {
+
+    private Long counter;
+    final private ParkhausServlet servlet = new ParkhausServlet();
+
+
+    @BeforeEach
+    void setUp(){
+    }
+
+    @AfterEach
+    void tearDown(){
+        this.counter = 0L;
+    }
+
+    @Test
+    @DisplayName("tests if getBill() equals body[4]")
+    void testGetBill(){
+    }
+
 }
