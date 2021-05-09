03d996de98e0f3e9432404d833ff66631bd231a7
1nbetw33n <a.ab@mail.de>
2021-05-09 09:58:56 +0200

    if("leave".equals(event))-condition added to getBill()-Surrogate. Created getTotalCars() with shallow body.

--- src/main/java/com/example/Parkhaus/ParkhausServletDummy.java
+++ src/main/java/com/example/Parkhaus/ParkhausServletDummy.java
@@ -5,15 +5,25 @@ public class ParkhausServletDummy {
     //getBill()-Surrogates for getBill()-Method in ParkhausServlet
     final protected static Double getBill(final String BODY){
         String[] params = BODY.split(",");
-        StringBuilder priceString = new StringBuilder();
-        priceString.append(params[4]);
-        Double price = Double.parseDouble(priceString.toString());
-        Double bill = price;
+        String event =    params[0];
+
+        Double price =    0.;
+        Double bill  =    0.;
+        if ("leave".equals(event)){
+            StringBuilder priceString = new StringBuilder();
+            priceString.append(params[4]);
+            price = Double.parseDouble(priceString.toString());
+            bill = price;
+        }
         return bill;
         //this method could be a one_liner like the return statement below,
         //but to reflect the steps performed in servlet we did it this way
 
         //return Double.parseDouble(BODY.split(",")[4]);
+    }
+
+    final protected static Long getTotalCars(final String BODY){
+        return 0L;
     }
 
 }
--- src/main/java/com/example/Parkhaus/Parkhaus_UnitTest.java
+++ src/main/java/com/example/Parkhaus/Parkhaus_UnitTest.java
@@ -7,14 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Parkhaus_UnitTest {
 
     private Long counter;
-    private String body;
+    private String body1;
+    private String body2;
     String[] params;
 
 
     @BeforeEach
     void setUp(){
-        this.body = "leave,955,1620544094579,7029,703,7f7530ab7d9b69ed3906d0e89509ccdb,#70fe5c,2,_,_,955";
-        this.params = body.split(",");
+        this.body1 = "leave,955,1620544094579,7029,703,7f7530ab7d9b69ed3906d0e89509ccdb,#70fe5c,2,_,_,955";
+        this.params = body1.split(",");
         this.counter = 0L;
     }
 
@@ -22,7 +23,7 @@ public class Parkhaus_UnitTest {
     @AfterEach
     void tearDown(){
         this.counter = 0L;
-        this.body = null;
+        this.body1 = null;
         this.params = null;
     }
 
@@ -30,7 +31,13 @@ public class Parkhaus_UnitTest {
     @Test
     @DisplayName("tests if getBill() equals body[4]")
     void testGetBill(){
-        assertEquals(Double.parseDouble(this.params[4]), ParkhausServletDummy.getBill(this.body), "no clue, how to test the servlet methods - no." + ++counter);
+        assertEquals(Double.parseDouble(this.params[4]), ParkhausServletDummy.getBill(this.body1), "damn! something works not like intended - no." + ++counter);
+    }
+
+    @Test
+    @DisplayName("tests if getTotalCars() count the leaving cars correctly (only increments when cars leaving)")
+    void testGetTotalCars(){
+
     }
 
 }
